a177b98b5840ffd597542dff98f14fae0bd44662
vanshikaaa <133578313+Vanshikaxxa@users.noreply.github.com>
2025-03-23 13:18:33 +0530

    Update main.ml

--- stars/bin/main.ml
+++ stars/bin/main.ml
@@ -1,6 +1,7 @@
 open Claudius
 open Tsdl
 
+
 type star = {
   x : int;
   y : int;
@@ -20,18 +21,24 @@ let render (_s : Screen.t) (stars : star list) : Primitives.t list =
     Primitives.Pixel ({ x = star.x; y = star.y }, star.brightness)
   ) stars
 
-let render_debug_info () =
-  let fps = 60 in
-  let resolution = "800x600" in
-  Sdl.log "FPS: %d | Resolution: %s" fps resolution  
+let last_time = ref (Int32.of_int 0)
+let fps = ref 0
+
+let render_debug_info (s : Screen.t) () =
+  let now = Sdl.get_ticks () in
+  let elapsed = Int32.sub now !last_time in
+  if elapsed > 01l then fps := 1000 / (Int32.to_int elapsed);  
+  last_time := now;
+  let width, height = Screen.dimensions s in
+  Sdl.log "FPS: %d | Resolution: %dx%d" !fps width height
 
 let tick (_t : int) (s : Screen.t) (prev : Framebuffer.t) (_inputs : Base.KeyCodeSet.t) : Framebuffer.t =
   let buffer = Framebuffer.map (fun _ -> 0) prev in
   let width, height = Screen.dimensions s in
   let stars = generate_stars 100 width height |> twinkle in
   render s stars |> Framebuffer.render buffer;
-  render_debug_info ();
+  render_debug_info s ();
   buffer
 
 let () =
-  Screen.create 800 600 1 (Palette.generate_mono_palette 16) |> Base.run "Twinkling Stars" None tick
+  Screen.create 800 640 1 (Palette.generate_mono_palette 16) |> Base.run "Twinkling Stars" None tick
